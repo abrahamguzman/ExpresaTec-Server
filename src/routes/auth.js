bc24db0bc3fc95410c5fa9319ef2a26fd60d7365
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2023-12-03 19:55:04 -0500

    Update auth.js
    Add comentary about the prev route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ const { login, register } = require('../controllers/auth.controller.js');
 const user = require("../controllers/user.controller.js");
 const router = express.Router();
 
+// api/v1/auth
 router.post('/login', login);
 router.post('/register', user.create);
 

8ff1605b3021bc7a903cfe8d698c82a006a16944
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2023-12-19 11:11:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 require('dotenv').config();
-const { json } = require('express');
 const cors = require('cors');
 const { port } = require("./src/config/global.config.js");
 const db = require("./src/models/index.js");
@@ -22,8 +21,14 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", require("./src/routes/auth.js"));
 app.use(authMiddleware);
 
+app.use("/api/v1/generalInfo", require("./src/routes/generalInfo.js"));
 app.use("/api/v1/users", require("./src/routes/user.js"));
+app.use("/api/v1/children", require("./src/routes/child.js"));
+app.use("/api/v1/categories", require("./src/routes/category.js"));
 app.use("/api/v1/games", require("./src/routes/game.js"));
+app.use("/api/v1/gameActivities", require("./src/routes/gameActivity.js"));
+app.use("/api/v1/emotions", require("./src/routes/emotion.js"));
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);

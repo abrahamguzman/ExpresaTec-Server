cd001fc0d624e2db83e5265d47f5fafc3d34f199
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2023-12-19 11:11:50 -0500

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,6 +3,12 @@ const USER = process.env.DB_USER;
 const PASSWORD = process.env.DB_PASSWORD;
 const DB = process.env.DB_NAME;
 const dialect = "mysql";
+const pool = {
+  max: 5,
+  min: 0,
+  acquire: 30000,
+  idle: 10000,
+};
 
 module.exports = {
   HOST,
@@ -10,4 +16,5 @@ module.exports = {
   PASSWORD,
   DB,
   dialect,
+  pool,
 };
